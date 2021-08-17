e3bcfebb24f150d1d61287d52ffd27571557359a
reymon359 <ramonmorcillocascales@gmail.com>
2021-08-17 22:36:01 +0200

    feat: add repo to docsify config

--- docsify.config.js
+++ docsify.config.js
@@ -2,6 +2,7 @@ module.exports = {
   main: {
     name: 'Collman fruits example',
     basePath: 'https://raw.githubusercontent.com/reymon359/collman-fruits-example/master/docs/', // To work with GitHub Pages
+    repo: 'https://github.com/reymon359/collman-fruits-example/',
     loadSidebar: true,
     alias: {
       '/.*/_sidebar.md': '/_sidebar.md'
