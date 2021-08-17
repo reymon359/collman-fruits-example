ace1c44a688fbd6d69c2ed8d4f504b6fd6ddc1c7
reymon359 <ramonmorcillocascales@gmail.com>
2021-08-17 21:56:40 +0200

    feat: add docsify config and readme plus update index.html

---
+++ README.md
@@ -0,0 +1,3 @@
+# Collman Fruits example
+
+This is an example repository of a [Collman](https://github.com/reymon359/collman) collection.
---
+++ docsify.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  main: {
+    name: 'Collman fruits example',
+    basePath: 'https://raw.githubusercontent.com/reymon359/collman-fruits-example/master/docs/', // To work with GitHub Pages
+    loadSidebar: true,
+    alias: {
+      '/.*/_sidebar.md': '/_sidebar.md'
+    },
+    subMaxLevel: 3,
+    sidebarDisplayLevel: 1,
+    search: 'auto'
+  },
+}
